ffbef2af077bfe6b85ba19c281ffe4c1e8c2d3d3
snake2k <none>
2017-06-30 19:49:38 -0400

    Staying with the normal stuff lol

--- cuppoc.py
+++ cuppoc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 import os
 import sys
-import urwid
 from time import sleep
 from pysqlcipher import dbapi2 as sqlite
 from pysqlcipher.dbapi2 import DatabaseError as PYSQLDBERR
@@ -46,36 +45,36 @@ def init_cuppoc_db():
     Use pysqlcipher to create an encrypted targets.db or
     open the existing one with the correct key.
     '''
-    clear()
-    print_separator()
-    print("NOTE:")
-    print("[!] Checking to see if \"targets.db\" exists or not.")
-    print("[!] If it doesn't, cuppoc will make one.")
-    print("[!] cuppoc uses AES256 to encrypt your local database.")
-    print("[!] This is due to Personally Identifiable Information (PII)")
-    print("[!] which will be stored in the database.\n")
-    print("[!] You will be asked to enter a password.")
-    print("[!] This password is the key to your database. Don't forget :P")
-    print("[!] If you have already done this before, " +
-          "please use the password you used before to access your db.")
-    db = sqlite.connect("./targets.db")
-    cursor = db.cursor()
-    user_key = raw_input("> Please enter the password for database: ")
-    cursor.execute("PRAGMA key='%s'" % user_key)
-    try:
-        cursor.execute("CREATE TABLE IF NOT EXISTS Targets (" +
-                       "ID INTEGER PRIMARY KEY," +
-                       "First_Name TEXT," +
-                       "Last_Name TEXT)")
-        print("[+] Done... Starting cuppoc!")
-        print_separator()
-        sleep(3)
-    except PYSQLDBERR:
-        print("[-] Database doesn't exist or is encrypted.")
-        print("[-] If database exists, enter the correct password.")
-        db.close()
-        print_separator()
-        sys.exit(0)
+    while True:
+        print("NOTE:")
+        print("[!] Checking to see if \"targets.db\" exists or not.")
+        print("[!] If it doesn't, cuppoc will make one.")
+        print("[!] cuppoc uses AES256 to encrypt your local database.")
+        print("[!] This is due to Personally Identifiable Information (PII)")
+        print("[!] which will be stored in the database.\n")
+        print("[!] You will be asked to enter a password.")
+        print("[!] This password is the key to your database. Don't forget :P")
+        print("[!] If you have already done this before,")
+        print("[!] Then please use the password you used before.")
+        db = sqlite.connect("./targets.db")
+        cursor = db.cursor()
+        user_key = raw_input("> Please enter the password for database: ")
+        cursor.execute("PRAGMA key='%s'" % user_key)
+        try:
+            cursor.execute("CREATE TABLE IF NOT EXISTS Targets (" +
+                           "ID INTEGER PRIMARY KEY," +
+                           "First_Name TEXT," +
+                           "Last_Name TEXT)")
+            print("[+] Done... Starting cuppoc!")
+            print_separator()
+            sleep(3)
+            break
+        except PYSQLDBERR:
+            print("[-] Database doesn't exist or is encrypted.")
+            print("[-] If database exists, enter the correct password.")
+            print_separator()
+            db.close()
+            sys.exit(0)
     return [db, cursor]
 
 
@@ -118,56 +117,52 @@ def delete_target(db, cursor):
     print("Delete targets under construction")
 
 
-def menu(title, choices):
-    body = [urwid.Text(title), urwid.Divider()]
-    for c in choices:
-        button = urwid.Button(c)
-        urwid.connect_signal(button, 'click', item_chosen, c)
-        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
-
-
-def item_chosen(button, choice):
-    response = urwid.Text([u'You chose ', choice, u'\n'])
-    done = urwid.Button(u'Ok')
-    urwid.connect_signal(done, 'click', exit_program)
-    main.original_widget = urwid.Filler(
-        urwid.Pile(
-            [response,
-             urwid.AttrMap(done, None, focus_map='reversed')]))
-
-
-def exit_program(button):
-    raise urwid.ExitMainLoop()
+def exit_program(db, cursor):
+    '''
+    The most complicated of all the available functions!
+    '''
+    print("Exiting program...")
+    db.close()
+    sys.exit(1)
 
 
 def print_separator():
     '''
     Prints a separator depending on the terminal's width.
     '''
-    SEPARATOR_SYMBOL = "-"
-    SEPARATOR_LENGTH = os.popen('stty size', 'r').read().split()[-1]
     print(str(SEPARATOR_SYMBOL) * int(SEPARATOR_LENGTH))
 
 
+SEPARATOR_SYMBOL = "-"
+SEPARATOR_LENGTH = os.popen('stty size', 'r').read().split()[-1]
+
 if __name__ == "__main__":
+    clear()
+    print_separator()
+
     db_objs = init_cuppoc_db()
     db_conn = db_objs[0]
     db_cursor = db_objs[1]
 
-    choices = ["Create a new target.",
-               "List all targets.",
-               "Load target.",
-               "Delete target.",
-               "Exit"]
-    main = urwid.Padding(
-        menu(u'Welcome to the Common User Password Profiler (cuppoc)!',
-             choices),
-        left=2, right=2)
-    top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
-                        align='center', width=('relative', 60),
-                        valign='middle', height=('relative', 60),
-                        min_width=20, min_height=9)
-    urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
-    db_conn.close()
-    print("[!] Closed connection to db.")
+    main_menu = {1: create_target,
+                 2: list_targets,
+                 3: load_target,
+                 4: delete_target,
+                 5: exit_program}
+    while True:
+        clear()
+        print_separator()
+        print("Welcome to Common User Password Profiler On Crack (CUPPOC)!")
+        print("1. Create a new target")
+        print("2. List targets")
+        print("3. Load target")
+        print("4. Delete target")
+        print("5. Exit")
+        try:
+            choice = main_menu[input("> ")]
+            print_separator()
+            choice(db_conn, db_cursor)
+        except KeyError:
+            print("Invalid selection. Returning to main menu.")
+            print_separator()
+            sleep(3)
